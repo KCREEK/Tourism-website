077697821c698852057af18dd98757c32df0e2e8
Siddhant Rakibe <102241647+KCREEK@users.noreply.github.com>
2023-04-10 00:07:18 +0530

    updated `medhavin` info

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const header = document.querySelector("header");
+/*const header = document.querySelector("header");
 
 window.addEventListener ("scroll", function() {
 	header.classList.toggle ("sticky", window.scrollY > 0);
